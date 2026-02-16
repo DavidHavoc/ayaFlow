aa5abe6e6608814817884e5038d955f38dd1ba11
DavidHavoc <data@zhorzholiani.com>
2026-02-16 15:31:00 +0100

    qdisc fix

--- ayaflow/src/main.rs
+++ ayaflow/src/main.rs
@@ -66,8 +66,7 @@ async fn main() -> anyhow::Result<()> {
 
     // If the clsact qdisc already exists (EEXIST), that is fine.
     if let Err(e) = tc::qdisc_add_clsact(iface) {
-        let io_err = e.downcast_ref::<std::io::Error>();
-        if io_err.map_or(true, |ie| ie.raw_os_error() != Some(17)) {
+        if e.raw_os_error() != Some(17) {
             return Err(e.into());
         }
         tracing::debug!("clsact qdisc already exists on {}, reusing", iface);
